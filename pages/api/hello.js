fdeb226293b872283f8e8ce5d28c834e66cd21bf
unknown <mohammadmehdiyousefi02@gmail.com>
2022-09-16 19:20:34 +0430

    socketio-app

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Server } from "socket.io"
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.on('added', msg => {
+        socket.broadcast.emit('upd', msg)
+      })
+    })
+  }
+  res.end()
 }
